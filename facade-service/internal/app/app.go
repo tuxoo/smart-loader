2f902b91aa3a2941222949dbc34dcee3c7bfb5bb
Eugene Krivtsov <krivtsov.eugene@gmail.com>
2022-11-26 16:38:40 +0300

    SmartLoader: docker-compose, postgres and liquibase have been added;

--- facade-service/internal/app/app.go
+++ facade-service/internal/app/app.go
@@ -1,5 +1,24 @@
 package app
 
-func Run(configPath string) {
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/tuxoo/smart-loader/facade-service/internal/config"
+)
 
+func Run(configPath string) {
+	fmt.Println(`
+ ####~~##~~~#~~####~~#####~~######~~~~##~~~~~~####~~~####~~#####~~#####~#####
+##~~~~~###~##~##~~##~##~~##~~~##~~~~~~##~~~~~##~~##~##~~##~##~~##~##~~~~##~~##
+ ####~~##~#~#~######~#####~~~~##~~~~~~##~~~~~##~~##~######~##~~##~####~~#####
+~~~~##~##~~~#~##~~##~##~~##~~~##~~~~~~##~~~~~##~~##~##~~##~##~~##~##~~~~##~~##
+ ####~~##~~~#~##~~##~##~~##~~~##~~~~~~######~~####~~##~~##~#####~~#####~##~~##
+	`)
+
+	_, err := config.InitConfig(configPath)
+	if err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	logrus.Println("Hello")
 }
---
+++ facade-service/internal/config/config.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"strings"
+	"time"
+)
+
+const (
+	defaultHttpPort           = "9000"
+	defaultHttpRWTimeout      = 10 * time.Second
+	defaultMaxHeaderMegabytes = 1
+)
+
+type (
+	Config struct {
+		HTTP HTTPConfig
+	}
+
+	HTTPConfig struct {
+		Host               string        `mapstructure:"host"`
+		Port               string        `mapstructure:"port"`
+		ReadTimeout        time.Duration `mapstructure:"readTimeout"`
+		WriteTimeout       time.Duration `mapstructure:"writeTimeout"`
+		MaxHeaderMegabytes int           `mapstructure:"maxHeaderMegabytes"`
+	}
+)
+
+func InitConfig(path string) (*Config, error) {
+	viper.AutomaticEnv()
+	preDefaults()
+
+	if err := parseConfigFile(path); err != nil {
+		return nil, err
+	}
+
+	if err := parseEnv(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := unmarshalConfig(&cfg); err != nil {
+		return nil, err
+	}
+
+	setFromEnv(&cfg)
+
+	return &cfg, nil
+}
+
+func preDefaults() {
+	viper.SetDefault("http.port", defaultHttpPort)
+	viper.SetDefault("http.max_header_megabytes", defaultMaxHeaderMegabytes)
+	viper.SetDefault("http.timeouts.read", defaultHttpRWTimeout)
+	viper.SetDefault("http.timeouts.write", defaultHttpRWTimeout)
+}
+
+func parseConfigFile(filepath string) error {
+	path := strings.Split(filepath, "/")
+
+	viper.AddConfigPath(path[0])
+	viper.SetConfigName(path[1])
+
+	return viper.ReadInConfig()
+}
+
+func parseEnv() error {
+	if err := parseHttpEnv(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseHttpEnv() error {
+	if err := viper.BindEnv("http.host", "HTTP_HOST"); err != nil {
+		return err
+	}
+
+	return viper.BindEnv("http.port", "HTTP_PORT")
+}
+
+func unmarshalConfig(cfg *Config) error {
+	if err := viper.UnmarshalKey("http", &cfg.HTTP); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setFromEnv(cfg *Config) {
+	cfg.HTTP.Host = viper.GetString("http.host")
+	cfg.HTTP.Port = viper.GetString("http.port")
+}
